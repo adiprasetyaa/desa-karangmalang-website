4ab56056d2712911d4dc10df54f9829325e72779
Afif Nur Fauzi <alscheift@gmail.com>
2024-07-22 19:10:51 +0700

    ui: individu pages

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -12,7 +12,9 @@ class PeopleController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.people.index', [
+            'peoples' => People::all()
+        ]);
     }
 
     /**
