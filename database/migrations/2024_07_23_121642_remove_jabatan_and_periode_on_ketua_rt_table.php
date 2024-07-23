ea964efb2f9a293f6fd55b869facfea58b5eb051
Afif Nur Fauzi <alscheift@gmail.com>
2024-07-23 19:26:39 +0700

    migration: remove jabatan on ketua_rt table

---
+++ database/migrations/2024_07_23_121642_remove_jabatan_and_periode_on_ketua_rt_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // remove periodestart and end
+        Schema::table('ketua_rt', function (Blueprint $table) {
+            $table->dropColumn('Jabatan');
+            $table->dropColumn('PeriodeStart');
+            $table->dropColumn('PeriodeEnd');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('ketua_rt', function (Blueprint $table) {
+            $table->string('Jabatan');
+            $table->date('PeriodeStart');
+            $table->date('PeriodeEnd');
+        });
+    }
+};
