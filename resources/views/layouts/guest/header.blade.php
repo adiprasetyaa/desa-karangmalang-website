c77202a62371cc81fd35a6f0c3852e5b78509dc3
adiprasetya <adiprasetyaa11@gmail.com>
2024-08-18 13:05:08 +0700

    Guest: Add Sub-Lembaga Desa in Navbar

--- resources/views/layouts/guest/header.blade.php
+++ resources/views/layouts/guest/header.blade.php
@@ -47,7 +47,16 @@
                                         <ul class="dropdown">
                                             <li><a href="portfolio.html">Struktur Organisasi</a></li>
                                             <li><a href="single-portfolio.html">Perangkat Desa</a></li>
-                                            <li><a href="single-portfolio.html">Lembaga Desa</a></li>
+                                            <li><a href="single-portfolio.html">Lembaga Desa</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="{{ route('guest.pemerintahan.lembaga_desa.bpd') }}">BPD</a></li>
+                                                    <li><a href="{{ route('guest.pemerintahan.lembaga_desa.bumdes') }}">BUMDES</a></li>
+                                                    <li><a href="{{ route('guest.pemerintahan.lembaga_desa.linmas') }}">LINMAS</a></li>
+                                                    <li><a href="{{ route('guest.pemerintahan.lembaga_desa.lkd') }}">LKD</a></li>
+                                                    <li><a href="{{ route('guest.pemerintahan.lembaga_desa.lpmd') }}">LPMD</a></li>
+                                                    <li><a href="{{ route('guest.pemerintahan.lembaga_desa.spmdes') }}">SPMDES</a></li>
+                                                </ul>
+                                            </li>
                                         </ul>
                                     </li>
                                     <li><a href="blog.html">Informasi Publik</a>
